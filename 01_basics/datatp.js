79e3eb1e695a65d3af298dc1d53f0d0df8c7a5dc
mansi-g143 <153274801+mansi-g143@users.noreply.github.com>
2025-12-21 16:06:47 +0000

    Datatypes

---
+++ 01_basics/datatp.js
@@ -0,0 +1,13 @@
+"use strict"; // use newer version of javastrict
+
+// alert(3+3); this can be used in external browser but not here bc here we use node.js
+let age=24;
+let name="Mansi";
+let sport;
+
+// different datatypes like boolean, numbers, strings etc
+// null is a standalone value 
+ 
+console.log(typeof age) // integer
+console.log(typeof null) // object; null is an object
+console.log(typeof undefined) //undefined
