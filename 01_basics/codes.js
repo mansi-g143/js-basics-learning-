0c17a9fad5906c4d496e1f57d95580572266f2d7
mansi-g143 <153274801+mansi-g143@users.noreply.github.com>
2025-12-23 08:16:07 +0000

    if else and loop

---
+++ 01_basics/codes.js
@@ -0,0 +1,17 @@
+console.log("MANSI GUPTA")
+
+
+let person="female";
+    if (person="female"){
+        console.log("Hello,I am Mansi");
+    } else {
+        console.log("Hello this person is male")
+    }
+
+
+let n=0;
+
+for (i=0; i<=1000; i++){
+    n=n+i;
+}
+console.log(n)
