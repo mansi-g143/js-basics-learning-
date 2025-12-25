df1546cae1558f2f67864dfc3f5041c00ac968d9
mansi-g143 <153274801+mansi-g143@users.noreply.github.com>
2025-12-25 12:11:43 +0000

    reversing a array

---
+++ 01_basics/revarray.js
@@ -0,0 +1,8 @@
+const number=[1,2,3,4,5];
+let reverse=[];
+ for(let i=0; i<number.length; i++){
+    reverse[i]=number[number.length-1-i];} //agar idhr i nhi minus karte toh woh same number repeat hoga
+    
+console.log(reverse);
+
+
