f2676ec9a062a0cf1a425fb3457bc98ed47d4bfb
mansi-g143 <153274801+mansi-g143@users.noreply.github.com>
2025-12-26 12:43:20 +0000

    function

--- 01_basics/func.js
+++ 01_basics/func.js
@@ -3,3 +3,16 @@ function sum(a,b) {
 }
 
 console.log(sum (3,4))
+
+function vowel(x){
+    let count=0;
+    for(let i=0; i<x.length; i++){
+        if(x[i]==="a" || x[i]==="e" ||x[i]==="i" ||x[i]==="o" ||x[i]==="u"){
+            count++;
+        
+        }
+    }console.log(count)
+}
+
+vowel("mansi")
+
