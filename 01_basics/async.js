4f09188d809f2184155eef74daf9f7eddf6d9bc9
mansi-g143 <153274801+mansi-g143@users.noreply.github.com>
2026-01-07 16:34:03 +0000

    asynchronous function

--- 01_basics/async.js
+++ 01_basics/async.js
@@ -8,3 +8,19 @@ function myFunction(callback) {
 myFunction((data) => {
     console.log("Data:", data);
 });
+
+
+function findSum(n){
+    let ans=0;
+    for(let i=0; i<n;i++){
+        ans=ans+i;
+    }
+    return ans;
+}
+
+function findSumtill100(){
+    console.log(findSum(100))
+}
+
+setTimeout(findSumtill100 , 1000)
+console.log("hello world");
